076e8008272cf08fb1316d6980abb0b72556f1cc
sumanthdevopsgit <93962528+sumanthdevopsgit@users.noreply.github.com>
2024-12-02 14:59:50 +0530

    Add files via upload

---
+++ modules/secrets-manager/variables.tf
@@ -0,0 +1,15 @@
+variable "secret-data" {
+
+    description = "Adding a admin user"
+    type = string
+    default = "https://test-url.com"
+
+}
+
+
+variable "workload-project-name" {
+
+  description = "this is the service project"
+  type = string
+
+}
