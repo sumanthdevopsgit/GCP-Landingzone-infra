e7d10738e246995a27a2ecce3713c4bf844dd663
sumanthdevopsgit <93962528+sumanthdevopsgit@users.noreply.github.com>
2024-12-02 14:56:54 +0530

    Add files via upload

---
+++ modules/gcp-required-api's/variables.tf
@@ -0,0 +1,13 @@
+variable "workload-project-name" {
+
+  description = "this is the service project"
+  type = string
+
+}
+
+variable "network-project-name" {
+
+  description = "This is the host project"
+  type = string
+
+}
