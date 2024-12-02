1c3b3cde021043374f59d9f24378c88f90815e4a
sumanthdevopsgit <93962528+sumanthdevopsgit@users.noreply.github.com>
2024-12-02 15:03:39 +0530

    Add files via upload

---
+++ modules/vpc-peering/variables.tf
@@ -0,0 +1,32 @@
+#variable "shared-network" {
+#  description = "Testing of the network"
+#  type        = string
+#}
+
+#variable "service-network" {
+#  description = "Testing of the network"
+#  type        = string
+#}
+
+variable "shared-vpc-name" {
+  description = "shared network name"
+  type        = string
+}
+
+variable "service-vpc-name" {
+  description = "service network name"
+  type        = string
+
+}
+
+
+#variable "shared-network-name" {
+#  description = "The name of the shared network being created"
+#  type        = string
+#}
+
+#variable "service-network-name" {
+#  description = "The name of the network being created in service project"
+#  type        = string
+
+#}
