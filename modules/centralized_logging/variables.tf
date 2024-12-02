d6869ab129fa2d24c0a3e75d3929aea03a9471b5
sumanthdevopsgit <93962528+sumanthdevopsgit@users.noreply.github.com>
2024-12-02 14:51:14 +0530

    Add files via upload

---
+++ modules/centralized_logging/variables.tf
@@ -0,0 +1,20 @@
+variable "workload-project-name" {
+
+  description = "this is the service project"
+  type = string
+
+}
+
+variable "network-project-name" {
+
+  description = "This is the host project"
+  type = string
+
+}
+
+#variable "centralized-logging-bucket" {
+
+#  description = "Enter the centralized bucket name to store the logs"
+#  type = string
+
+#}
